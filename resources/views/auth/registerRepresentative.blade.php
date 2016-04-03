e6270a0ac4509d0f326aa6e6da8e250cf544da46
Jahzeel Coss <gear_jahzeel24@hotmail.com>
2016-04-03 15:01:45 -0500

    vista registrar organizador

--- resources/views/auth/registerRepresentative.blade.php
+++ resources/views/auth/registerRepresentative.blade.php
@@ -1,44 +1,69 @@
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        Han habido problemas con sus datos.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<form method="POST" action="/auth/register">
-    {!! csrf_field() !!}
+@extends('layouts.base')
+@section('body')
+    <br><br><br><br><br><br>
+    <section id="port-content">
+        <div class="container">
+            <div class="row"><!--  título -->
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="feature_header text-center">
+                        <h3 class="feature_title"><b>Registrate</b></h3>
+                        <h4 class="feature_sub"></h4>
+                        <div class="divider"></div>
+                    </div>
+                </div>
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        Han habido problemas con sus datos.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif               
+            </div>
+            <div class="row">
+                <div class="contact_full">
+                    <form method="POST" action="/auth/register">
+                        <div class="col-md-6 left">
+                            <div class="left_contact">
+                                {!! csrf_field() !!}
+                                <div class="form-level">
+                                    <input type="text" name="first_name" value="{{ old('first_name') }}" placeholder="Nombre(s)">
+                                    <span class="form-icon fa fa-user"></span>
+                                </div>
+                               <div class="form-level">
+                                    <input type="text" name="last_name" value="{{ old('last_name') }}" placeholder="Apellido(s)">
+                                    <span class="form-icon fa fa-user"></span>
+                                </div>
+                                <div class="form-level">
+                                    <input type="email" name="email" value="{{ old('email') }}" placeholder="Correo Electrónico">
+                                    <span class="form-icon fa fa-envelope-o"></span>
+                                </div>                           
 
-    <div>
-        Nombres:
-        <input type="text" name="first_name" value="{{ old('first_name') }}">
-    </div>
+                                                                                   
+                            </div>
+                        </div>
+                        <div class="col-md-6 right">
+                            <div class="form-level">
+                                <input type="password" name="password" placeholder="Contraseña">
+                                <span class="form-icon fa fa-lock"></span>
+                            </div>
+                            <div class="form-level">
+                                <input type="password" name="password_confirmation" placeholder="Confirmar Contraseña">
+                                <span class="form-icon fa fa-lock"></span>
+                            </div>                             
 
-    <div>
-        Apellidos:
-        <input type="text" name="last_name" value="{{ old('last_name') }}">
-    </div>
+                             <input type="hidden" name="is_representative" value="true" />
+                        </div>
+                        <div class="col-md-12 text-center">
+                             <button class="btn btn-main featured" type="submit">Registrarme</button>
+                        </div>                        
+                    </form>   
+                </div>
+            </div>
+    
+        </div>
+    </section>    
 
-    <div>
-        Email:
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        Contraseña:
-        <input type="password" name="password">
-    </div>
-
-    <div>
-        Confirmar contraseña:
-        <input type="password" name="password_confirmation">
-    </div>
-
-     <input type="hidden" name="is_representative" value="true" />
-
-    <div>
-        <button type="submit">Registrarme</button>
-    </div>
-</form>
+@stop
