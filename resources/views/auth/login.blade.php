73356a460e317adaa2b727b19e44ba3062e6bd65
Jahzeel Coss <gear_jahzeel24@hotmail.com>
2016-04-06 14:59:45 -0500

    la vista del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,51 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Log in</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.5 -->
-    {{-- <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css"> --}}
-    <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" /> 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    {{-- <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css"> --}}
-    <link href="{{ asset('/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- iCheck -->
-    <link href="{{ asset('/plugins/iCheck/square/blue.css') }}" rel="stylesheet" type="text/css" />    
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="hold-transition login-page">
-    <div class="login-box">
-      <div class="login-logo">
-        <a href="../../index2.html"><b>YUCARRUN</b></a>
-      </div><!-- /.login-logo -->
-      <div class="login-box-body">
-        <p class="login-box-msg">Inicia Sesión</p>
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-              <strong>Error</strong> Hay problemas con sus datos.<br><br>
-              <ul>
-                @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-                @endforeach
-              </ul>
+
+@extends('layouts.base')
+@section('body')
+    <br><br><br><br><br><br>
+    <section id="port-content">
+        <div class="container">
+            <div class="row"><!--  título -->
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="feature_header text-center">
+                        <h3 class="feature_title"><b>Inicia Sesión</b></h3>
+                        <h4 class="feature_sub"></h4>
+                        <div class="divider"></div>
+                    </div>
+                </div>
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        Han habido problemas con sus datos.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif               
             </div>
-          @endif
-        <form method="POST" action="/auth/login">
-    	   {!! csrf_field() !!}         
-          <div class="form-group has-feedback">
-            <input type="email" class="form-control" placeholder="Correo" name="email"  value="{{ old('email') }}" required="true">
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Contraseña" name="password" id="password" required="true">
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-          <div class="row">
-            <div class="col-xs-8">
-              <div class="checkbox icheck">
-                <label>
-                  <input type="checkbox" name="remember"> Recordarme
-                </label>
-              </div>
-            </div><!-- /.col -->
-            <div class="col-xs-4">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">Iniciar</button>
-            </div><!-- /.col -->
-          </div>
-        </form>
+            <div class="row">
+              <div class="contact_full">
+                  <div class="col-sm-6 col-md-offset-3 text-center">
+                    <div class="left_contact">
+                        <form method="POST" action="/auth/login">
+                          {!! csrf_field() !!}         
+                          <div class="form-group has-feedback">
+                            <input type="email" class="form-control" placeholder="Correo" name="email"  value="{{ old('email') }}" required="true">                                    
+                          </div>
+                          <div class="form-group has-feedback">
+                            <input type="password" class="form-control" placeholder="Contraseña" name="password" id="password" required="true">                                   
+                          </div>
+                          <div class="row text-center">                                    
+                            <div class="col-xs-4 col-xs-offset-4">
+                              <button type="submit" class="btn btn-primary btn-block btn-flat">Iniciar</button>
+                            </div><!-- /.col -->
+                          </div>
+                        </form>
+                      </div>                                                      
+                </div>
+              </div>    
+            </div>
+    </section>    
 
-        
-
-        <a href="#">Olvide mi contraseña</a><br>
-        
-      </div><!-- /.login-box-body -->
-    </div><!-- /.login-box -->
-
-    <!-- jQuery 2.1.4 -->
-    <script src="{!! asset('/plugins/jQuery/jQuery-2.1.4.min.js') !!}"></script>   
-    <!-- Bootstrap 3.3.5 -->
-    <script src="{!! asset('/bootstrap/js/bootstrap.min.js') !!}"></script>      
-    <!-- iCheck -->
-    <script src="{!! asset('/plugins/iCheck/icheck.min.js') !!}"></script>     
-    
-    <script>
-      $(function () {
-        $('input').iCheck({
-          checkboxClass: 'icheckbox_square-blue',
-          radioClass: 'iradio_square-blue',
-          increaseArea: '20%' // optional
-        });
-      });
-    </script>
-  </body>
-</html>
+@stop
